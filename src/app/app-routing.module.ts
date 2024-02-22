daca9f1c67d588180a6f6dbcfd998c3135ff07cb
Gopal Jaiswal <gopal.jaiswal@edfora.com>
2024-02-22 19:37:50 +0530

    homemodule begin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
