39a12388c579c1fbc46e41f521d05ed12579a550
Santosh Mishra <mishrasantosh2105@gmail.com>
2024-03-08 23:18:27 +0530

    partial google auth

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,8 @@
+declare var google :any;
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import '../svg-animation.js';
 import { FormGroupDirective } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,10 +11,33 @@ import { FormGroupDirective } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id: '785038016750-dfqnsdf42ho3icq8u4akh7vh0k6jur0o.apps.googleusercontent.com',
+      callback: (resp:any)=>this.handleLogin(resp)
+    });
+    google.accounts.id.renderButton(document.getElementById('google-btn'),{
+      // text: "signup_with",
+      type: "standard",
+      shape: "circle"
+      
+    })
+    google.accounts.id.prompt();
   }
+  private decodeToken(token:string){
+    return JSON.parse(atob(token.split(".")[1]));
+  }
+  handleLogin(response:any){
+    if(response){
+      const payload=this.decodeToken(response.credential);
+      sessionStorage.setItem("loggedInUser",JSON.stringify(payload));
+     
+      this.router.navigate(['/']);
+    }
+  }
+
 
   ngAfterViewInit() {
     setTimeout(()=>{
